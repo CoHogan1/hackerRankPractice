eb6e21ad43f4fbf9ade56e21226223f503861814
Conner Hogan <ConnerRHogan@gmail.com>
2021-08-30 03:16:48 -0600

    added text message len coder

---
+++ tutorials/splittingString.js
@@ -0,0 +1,35 @@
+// splitting strings.
+
+let string = `111fdhlaksjhf alksjdlhfakj laksjdfhl 222asjdhf aslaks hflskj alksf
+    alksjhflkaj 333dlakjshflak jshflks ajhf laksjhflksah 444flksahflksh flksjd hflkjs
+    alhfkjsaf 555akjdhfljah lkajsd flkaj  alksfla 66ksjflkaj wqiueoqiuewyr eoiq yreoi
+    oqiurepqoiwu 77nddoijn wiowowncndcowenf 99ncocdn`
+
+//console.log(string.length);
+
+
+function splitting(string){
+    let copy = string.substr()
+
+    let maxChar = 130
+
+    let len = Math.ceil(string.length / maxChar)
+
+    if (string.length < maxChar){
+        return string
+    }
+
+    let response = []
+
+    for (let i = 0; i < len; i++) {
+        let part = copy.substr(0, maxChar)
+        let seperator = `${i + 1}/${len}`
+        response.push(part, seperator)
+        //response.push(seperator)
+        copy = copy.slice(maxChar)
+    }
+    console.log(response)
+    return response
+}
+
+splitting(string)
