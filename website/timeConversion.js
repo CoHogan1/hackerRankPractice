16f9b149474e3ae3095201f3da82bd390b4ae24e
Conner Hogan <ConnerRHogan@gmail.com>
2021-08-31 06:43:36 -0600

    pushing everything

---
+++ website/timeConversion.js
@@ -0,0 +1,18 @@
+// convert 12 hour time to militarty time.
+
+
+let date = '12:00:00AM'
+let test2 = "1:00:00PM"
+
+
+
+function convert(time){
+
+
+
+
+
+}
+
+
+convert(date)
---
+++ website/vanityPhone.js
@@ -0,0 +1,25 @@
+// take a list of codes, and see if the numbers in the number array contanes the codes.
+
+let numbers = ['+1234567891', '+9874563737', '+8264382424', '+5674678388']
+
+let codes = [5, 'bob', 'win', 'hello', 6]
+
+
+// abc = 2
+// def = 3
+// ghi = 4
+// jkl = 5
+// mnp = 6
+// pqrs = 7
+// tuv = 8
+// wxyz = 9
+
+
+
+function compare(nums, text){
+
+
+}
+
+
+compare(numbers, codes)
