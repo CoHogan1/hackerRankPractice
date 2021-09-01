c9ea50d59da83be8c1165e42119646ff57e43d7e
Conner Hogan <ConnerRHogan@gmail.com>
2021-08-31 22:24:58 -0600

    finished standard to military time

--- website/timeConversion.js
+++ website/timeConversion.js
@@ -1,18 +1,42 @@
 // convert 12 hour time to militarty time.
 
+let noon = '12:00:00AM'
 
-let date = '12:00:00AM'
-let test2 = "1:00:00PM"
+let midnight = '12:00:00PM'
 
+let pm = "01:20:10PM"
 
-
-function convert(time){
-
-
+let am = "01:01:00AM"
 
 
 
+function convert(s){
+
+    if (s.includes('AM')){
+        let militaryTime = s.slice(0, -2)
+        militaryTime.replace('12','00')
+        console.log(militaryTime)
+        return militaryTime
+    }
+
+    if (s.includes('PM')){
+        // remove the pm
+        let militaryTime = s.slice(0, -2)
+        // get the first 2 strings- the hour.
+        let hour = (militaryTime.slice(0, 2))
+        // add 12 to conver hour to military time.
+        let intHour = parseInt(hour) + 12
+        console.log(`${intHour}${militaryTime.slice(2, militaryTime.length)}`)
+        return `${intHour}${militaryTime.slice(2, militaryTime.length)}`
+    }
 }
 
 
-convert(date)
+convert(pm)
+console.log("pm");
+convert(am)
+console.log("am");
+convert(noon)
+console.log("noon");
+convert(midnight)
+console.log('midnight');
