ab0f65dc1a0a4e64686551ca0905dd6b617468b3
Conner Hogan <ConnerRHogan@gmail.com>
2021-09-13 22:20:34 -0600

    need to test XORstring2

---
+++ website/XORstrings2.js
@@ -0,0 +1,29 @@
+// given two int strings, find the XOR of them both.
+// input   10101
+//         00101
+
+// result  10000
+
+let test  = '10101'
+let test2 = '00101'
+let res   =  10000
+
+function string_xor(one, two){
+    let answer = ''
+    for (let i = 0; i < one.length; i++) {
+        if (one[i] === two[i]){
+            answer += '0'
+        } else {
+            answer += '1'
+        }
+    }
+    console.log(answer)
+    return answer
+}
+
+string_xor(test,test2)
+// if(one[i] === two[i]){
+//     answer += '0'
+// } else {
+//     answer += '1'
+// }
