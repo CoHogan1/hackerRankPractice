e918730aef8d4773eb31280f54b81f22f9cc2cf3
Conner Hogan <ConnerRHogan@gmail.com>
2022-02-22 01:47:28 -0700

    added merge sort

---
+++ tutorials/mergeSort.js
@@ -0,0 +1,24 @@
+
+function mergeSort(left, right){
+    const array = []
+    while(left.length && right.length){
+        if (left[0] < right[0]){
+            array.push( left.shift())
+        } else {
+            array.push(right.shift())
+        }
+    }
+    return array.concat(left.slice()).concat(right.slice())
+}
+
+const merge = (arr) => {
+    if (arr.length === 1) { return arr}
+    let mid = Math.ceil(arr.length / 2) // floor right is bigger arr.
+    let left = arr.slice(0,mid)
+    let right = arr.slice(mid)
+    return mergeSort(merge(left),merge(right))
+}
+
+let test = [1,3,5,7,9,0,8,6,4,2]
+
+console.log(merge(test))
--- tutorials/quickSory.js
+++ tutorials/quickSory.js
@@ -18,4 +18,40 @@ const quicksort = (arr) => {
 let test = [1,4,3,5,6,7,8,2,3,4,2,1,2,4,5,11,10,100]
 
 
-console.log(quicksort(test))
+// console.log(quicksort(test))
+//
+// let arr = [1,2,3,4,5]
+// let mid = Math.ceil(arr.length / 2)
+// let left = arr.slice(0,mid)
+// let right = arr.slice(mid)
+//
+// console.log(left, right);
+
+function merge(left, right){
+    let sorted = []
+
+    while(left.length && right.length){
+        if(left[0] > right[0]){
+            sorted.push(left.shift())
+        } else {
+            sorted.push(right.shift())
+        }
+    }
+    return [...sorted,...left,...right]
+}
+
+
+const counting = (a) => {
+    if (a.length === 0){ return a}
+    let mid = Math.ceil(a.length / 2)
+    let left = a.slice(0,mid)
+    let right = a.slice(mid)
+    return merge(
+        counting(left),
+        counting(right)
+    )
+}
+
+
+console.log(counting([1,2,3,4,5,10]), 0);
+console.log(counting([2,1,3]), 1);
