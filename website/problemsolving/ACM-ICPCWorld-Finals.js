e01ee98b2015e0cc024142100a5ae33d436ce34e
Conner Hogan <ConnerRHogan@gmail.com>
2022-04-13 02:13:08 -0600

    acmicpi solution added

--- website/problemsolving/ACM-ICPCWorld-Finals.js
+++ website/problemsolving/ACM-ICPCWorld-Finals.js
@@ -1,47 +1,26 @@
 
 function acmteams(topic){
-    let skills = []
-    let test = topic.split(' ')
-
-    // for (let a = 0; a < topic.length; a++) {
-    //     for (let z = 0; z < topic[a].length; z++){
-    //         console.log(topic[a][a], topic[z][z])
-    //     }
-    // }
-
-
-    return 'IDK man'
-    //[ [ 0, 2, 4 ], [ 0, 1, 2, 3 ], [ 3 ] ]
-
+    let num = 0
+    let max = 0
+    let group = 0
+    for (let i = 0; i < topic.length; i++) {
+        for (let j = i+1; j < topic.length; j++) {
+            for (let k = 0; k < topic[i].length; k++) {
+                if (parseInt(topic[i][k]) + parseInt(topic[j][k]) > 0){
+                    num++
+                }
+            }
+            if (max < num){
+                max = num
+                group = 1
+            } else if (max == num && num != 0){
+                group++
+            }
+            num = 0
+        }
+    }
+    return [max, group]
 }
 
-//console.log(acmteams(['10101','11110','00010']))
+console.log(acmteams(['10101','11110','00010']))
 console.log(acmteams(['10101','11100','11010','00101']))
-
-
-// let x = Array.from(Array(10).keys())
-//
-// console.log(x);
-
-
-// get the teams with most knowledge, and something else. lol
-
-
-// 1 0 1 0 1
-// 1 1 1 1 0
-// 0 0 0 1 0
-
-// t1 (1,2) [1,2,3,4,5]
-
-// t2 (1,3) [1,3,4,5]
-
-// t3 (2,3) [1,2,3,4]
-
-
-// arr.len =  number of teams
-// arr[i] =  number of topics
-
-// get each teams info.
-
-// t1 = arr[0] = 10101
-//               0 2 4
--- website/problemsolving/fairRations.js
+++ website/problemsolving/fairRations.js
@@ -1,18 +1,31 @@
 
 function fairRations(B){
-    let distributions = 0
-
-    B.forEach((per, ind)=>{
-        if (per % 2 != 0){ // odd
-            B[ind]++
-            B[ind + 1]++
-            distributions += 2
+    let pure = [...B]
+    let mod = B.map(x => x % 2)
+    let counter = 0
+    for (let i = 0; i < mod.length; i++) {
+        if (mod[i] === 1){
+            counter++
         }
-    })
-    console.log(B, " end result")
-
-    return distributions
+    }
+    if (counter % 2 != 0) {
+        return "NO"
+    }
+    counter = 0
+    while (pure.evry(x => x % 2 === 1)){
+        pure[counter]++
+        pure[counter + 1] ++
+        counter++
+    }
+    return counter
 }
 
 console.log(fairRations([4,5,6,7]))
 console.log(fairRations([2,3,4,5,6]),4)
+console.log(fairRations([1,2]),"NO")
+
+
+let x = [ 2,2,4]
+
+
+console.log(x.every(x => x % 2 === 0));
