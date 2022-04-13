b714b21cb9bfad3e0c8accf26bc3c09b0b660502
Conner Hogan <ConnerRHogan@gmail.com>
2022-04-13 02:29:53 -0600

    fair rations solution added

--- website/problemsolving/fairRations.js
+++ website/problemsolving/fairRations.js
@@ -1,31 +1,23 @@
 
 function fairRations(B){
-    let pure = [...B]
-    let mod = B.map(x => x % 2)
-    let counter = 0
-    for (let i = 0; i < mod.length; i++) {
-        if (mod[i] === 1){
-            counter++
+    let loaves = 0
+    let sub = B.length
+
+    for (let i = 0; i < B.length - 1; i++) {
+        if (B[i] % 2 === 1){
+            B[i]++
+            B[i+1]++
+            loaves += 2
         }
     }
-    if (counter % 2 != 0) {
+
+    if (B[sub -1] % 2 === 0 ){
+        return loaves
+    } else {
         return "NO"
     }
-    counter = 0
-    while (pure.evry(x => x % 2 === 1)){
-        pure[counter]++
-        pure[counter + 1] ++
-        counter++
-    }
-    return counter
 }
 
 console.log(fairRations([4,5,6,7]))
 console.log(fairRations([2,3,4,5,6]),4)
 console.log(fairRations([1,2]),"NO")
-
-
-let x = [ 2,2,4]
-
-
-console.log(x.every(x => x % 2 === 0));
