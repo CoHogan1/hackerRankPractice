975ba1a5681cbe28a14f38616a6abf9759dc96f7
Conner Hogan <ConnerRHogan@gmail.com>
2022-05-02 03:57:28 -0600

    sherlock and valid solution added

---
+++ website/problemsolving/sherlockAndValidString.js
@@ -0,0 +1,16 @@
+function isValid(s){
+    let memo = {}
+    s.split('').map(x => memo[x] ? memo[x]++ : memo[x] = 1)
+
+    let arr = Object.values(memo)
+    let count = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        if ( arr[i] != arr[0] ) { count++ }
+        if (count > 1){ return "NO"}
+    }
+    return "YES"
+}
+
+console.log(isValid('aabbcd'))
+//console.log(isValid('abcdefghhgfedecba'))
