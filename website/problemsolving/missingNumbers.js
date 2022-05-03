59307e918787db4be2edb1a9a5611fc78ce42641
Conner Hogan <ConnerRHogan@gmail.com>
2022-05-03 02:48:41 -0600

    sherlock and array solution added and missing numbers solution added

---
+++ website/problemsolving/missingNumbers.js
@@ -0,0 +1,16 @@
+function missingNumbers(arr, brr){
+    for (var i = 0; i < arr.length; i++) {
+        brr.splice(brr.indexOf(arr[i]), 1)
+        console.log(brr);
+    }
+    let ans = [...new Set(brr)]
+    return ans
+
+}
+
+console.log(missingNumbers([7,2,5,3,5,3],[7,2,5,4,6,3,5,3]))
+//console.log(missingNumbers([3,4,5,6,7,8,3,4,5,6],[3,4,4,5,6,7,5,8,3,6,5,6,4]));
+
+
+// 7,2,5,4,6,3,5,3
+// 7,2,5,3,5,3
--- website/week2/sherlockAndArray.js
+++ website/week2/sherlockAndArray.js
@@ -1,68 +1,22 @@
-// Watson gives Sherlock an array of integers. His challenge is to find an
-// element of the array such that the sum of all elements to the left is equal
-// to the sum of all elements to the right.
-
-let test  = [5,6,8,11]
-let test2 = [1,2,3,3]
-let idk =  [1,2,3]
-let idk2 = [1,2,3,3]
+// shelock and array.
 function balancedSums(arr){
-    if (arr.length === 0){return arr[0]}
-    let ans = false
-    arr.forEach((item, index)=>{
-        let left  = arr.slice(0, index).reduce((a, b) => { return a + b}, 0)
-        // right side of the array will be biger for first half.
-        let right = arr.slice(index + 1).reduce((a,b) => { return a + b}, 0)
-        if (left === right){
-            ans = true
+    let i = 0
+    let res = false
+    let before = arr.slice(0, i).reduce((a,b) => a-b, 0)
+    let after = arr.slice(i + 1).reduce((a,b) => a-b, 0)
+    while(!res && i < arr.length){
+        res = before == after
+        if (res){
+            return "YES"
+        } else {
+            before += arr[i]
+            after  -= arr[i + 1]
+            i++
         }
-        if (ans) {return }
-    })
-    if (ans){
-        return "YES"
-    } else {
-        return "NO"
     }
-}
-// console.log(balancedSums(test))
-// console.log(balancedSums(test2))
-// console.log(balancedSums(idk))
-// console.log(balancedSums(idk2))
-
-// other solution
-const arr = [1, 2, 3, 4, 5, 7, 3];
-const arr2 = [4, 6, 3, 4, 5, 2, 1];
-
-const isSherlockArray = arr => {
-   let sum = arr.reduce((acc, val) => acc+val);
-   let leftSum = 0;
-   for(let i = 0; i < arr.length; i++){
-      sum -= arr[i];
-      if(sum === leftSum){
-         return "YES";
-      };
-      leftSum += arr[i];
-   };
-   return "NO";
-};
-// console.log(isSherlockArray(arr));
-// console.log(isSherlockArray(arr2));
-
-// this one works within the time limie.
-function newAndImp(arr){
-    let sum = arr.reduce((acc, value)=> { return acc + value})
-    let leftSum = 0
-    let ans = false
-    arr.forEach((item, index)=>{
-        sum -= arr[index]
-        if (sum === leftSum){
-            ans = true
-            return
-        }
-        leftSum += arr[index]
-    })
-    return ( ans ? "YES" : "NO")
+    console.log(res);
+    return "NO"
 }
 
-console.log(newAndImp(idk) , " here");
-console.log(newAndImp(idk2), " yes");
+console.log(balancedSums([5,6,8,11]))
+console.log(balancedSums([1,1,4,1,1]))
