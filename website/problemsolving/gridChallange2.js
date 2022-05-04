bbfb295cd8ca652bcac47d55441927c94281102b
Conner Hogan <ConnerRHogan@gmail.com>
2022-05-04 09:22:46 -0600

    grid challange solution added

--- website/problemsolving/gridChallange2.js
+++ website/problemsolving/gridChallange2.js
@@ -1,16 +1,14 @@
-function gridChallange(arr){
-    let copy = []
-    // test for alpha sort
-    // make a copy of origional array
-    arr.map( x => copy.push(x.split('').sort().join('')) )
-
-
-
-
-
-
-
-    return copy
+function gridChallange(grid){
+    let copy = grid.map( x => x.split('').sort().join('') )
+    for (let i = 0; i < copy.length -1; i++) {
+        for (let z = 1; z < copy[0].length; z++) {
+            if (copy[i][z] > copy[i+1][z]){
+                return "NO"
+            }
+        }
+    }
+    return "YES"
 }
-
 console.log(gridChallange(['abc','ade','efg']))
+
+console.log(gridChallange(['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']))
