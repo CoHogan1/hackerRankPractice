efa5f8d8355ef549c280d65b7c7cf134e42c01aa
Conner Hogan <ConnerRHogan@gmail.com>
2022-05-14 21:01:28 -0600

    flipping bits solution added

--- website/problemsolving/flippingBits.js
+++ website/problemsolving/flippingBits.js
@@ -1,14 +1,12 @@
 function flippingBits(n){
-    let x = n.toString(2)
-
-    console.log(x)
-
-    let y = ''
-
-    for (let i = 0; i < x.length; i++) {
-        x[i] === '0' ?  y += "1" : y += "0"
-    }
-
-    return parseInt(y)
+    return (~n >>> 0)
 }
 console.log(flippingBits(2147483647))
+
+// why this works...
+// the '~' called tilde operator is the bitwise "Not" operator.
+// ex
+// not 1001 =
+//     0110
+// the tilde/ not operator, flips the bits into the opposite bits. 0 = 1, and 1 = 0
+// it uses two compliment a computer science term. google it. 
