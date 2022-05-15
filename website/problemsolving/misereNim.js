acdb269d4c82902c538c29996cfa8760a06ff1d6
Conner Hogan <ConnerRHogan@gmail.com>
2022-05-14 23:07:58 -0600

    misereNim solution added

--- website/problemsolving/misereNim.js
+++ website/problemsolving/misereNim.js
@@ -1,8 +1,17 @@
 function misereNum(s){
+    let xor = s[0]
+    let totalS = s[0]
 
+    for (let i = 1; i < s.length; i++) {
+        totalStones += s[i]
+        xor = xor ^ s[i]
+    }
 
+    if (totalS === s.length){
+        return totalS % 2 === 0 ? "First" : "Second"
+    }
 
-    return "Idk"
+    return xor ? "First" : 'Second'
 }
 
 console.log(misereNum(3))
