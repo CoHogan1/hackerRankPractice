3f2cd1d9d7664a055cc8d230ff86a1c173078d35
Conner Hogan <ConnerRHogan@gmail.com>
2022-06-19 19:31:36 -0600

    practice for google

--- googleInterview/treePrac.js
+++ googleInterview/treePrac.js
@@ -51,19 +51,19 @@ c.right = f;
 
 // }
 
-function depthFirst(root){
-    if(root === null){ return [] }
-    const result = []
-    const stack = [ root ]
-
-    while(stack.length > 0){
-        const current = stack.pop()
-        result.push(current.val)
-        if(current.right){ stack.push(current.right) }
-        if(current.left ){ stack.push(current.left ) }
-    }
-    return result
-}
+// function depthFirst(root){
+//     if(root === null){ return [] }
+//     const result = []
+//     const stack = [ root ]
+//
+//     while(stack.length > 0){
+//         const current = stack.pop()
+//         result.push(current.val)
+//         if(current.right){ stack.push(current.right) }
+//         if(current.left ){ stack.push(current.left ) }
+//     }
+//     return result
+// }
 
 // console.log(depthFirst(a))
 // console.log(depthFirst(null))
@@ -99,3 +99,22 @@ function breathFirst(root){
 
 
 // see if value exists in a tree
+
+
+
+function depthFirst(root){
+    if(root === null){ return [] }
+    const result = []
+    const stack = [ root ]
+
+    while(stack.length > 0){
+        const current = stack.pop()
+        result.push(current.val)
+        if(current.right){ stack.push(current.right) }
+        if(current.left ){ stack.push(current.left ) }
+    }
+    return result
+}
+
+
+depthFirst(a)
